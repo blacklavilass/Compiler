33e7d7bb1b6df2ef09b529547440ec20987a7c10
blacklavilass <vita20041129@yandex.ru>
2025-03-13 22:42:05 +0300

    Program node add

---
+++ src/main/java/nodes/ProgramNode.java
@@ -0,0 +1,28 @@
+package nodes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProgramNode extends BasicNode {
+    VariableNameNode name;
+    List<VarNode> vars;
+    StmtListNode stmts;
+
+    public ProgramNode(VariableNameNode name, List<VarNode> vars, StmtListNode stmts) {
+        this.name = name;
+        this.vars = vars;
+        this.stmts = stmts;
+    }
+    @Override
+    public List<? extends Node> children() {
+        List<Node> children = new ArrayList<>();
+        children.add(new ListNode(vars, "vars"));
+        children.add(stmts);
+        return children;
+    }
+
+    @Override
+    public String toString() {
+        return name == null ? "Program" : name.toString();
+    }
+}
